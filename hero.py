03f541ff6c3d9726b41a9b5065aeabe153e82c67
krishna1015 <krishna.khadka1015@gmail.com>
2019-01-27 23:02:43 +0545

    my first commit

---
+++ hero.py
@@ -0,0 +1,11 @@
+x=str(input("Name:"))
+y=int(input("Age:"))
+z=str(input("college Name:"))
+print("My Name is %s My Age is %s My College Name is %s" %(x,y,z))
+# total marks obtained
+a=int(input("marks of English:"))
+b=int(input("marks of Science:"))
+c=int(input("marks of Math:"))
+d=int(input("marks of Account:"))
+sum=a+b+c+d
+print("total marks is:%s" %(sum))
